dcfb38d324ca30a5340cc4b0c30424eb4dc88365
VictorSamu1 <victorsamuelizaias@gmail.com>
2026-06-22 19:45:43 -0300

    Atualizacao5
    Configuração do Nav.css para todos

--- app/login_screen/Js/cadastro.js
+++ app/login_screen/Js/cadastro.js
@@ -2,24 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("form");
 
     form.addEventListener("submit", (event) => {
-        // 1. Pegando os valores dos campos
+        // Aqui pega os valores dos campos 
         const nome = document.getElementById("nome").value;
         const senha = document.getElementById("senha").value;
 
-        // 2. Validações simples
+        //  Validações 
         if (nome.length < 3) {
             alert("O nome deve ter pelo menos 3 caracteres.");
-            event.preventDefault(); // Impede o envio
+            event.preventDefault(); 
             return;
         }
 
         if (senha.length < 8) {
             alert("Sua senha é muito curta. Use pelo menos 8 caracteres.");
-            event.preventDefault(); // Impede o envio
+            event.preventDefault(); 
             return;
         }
 
-        // Se passar por aqui, o formulário segue o curso normal
+        
         console.log("Cadastro realizado com sucesso para: " + nome);
 
         
