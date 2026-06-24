f78b1a380ebf3a6b0c4fdcd86eceb4094ab040ca
VictorSamu1 <victorsamuelizaias@gmail.com>
2026-06-23 21:28:43 -0300

    TestePHP2

--- app/login_screen/PHP_Login_screen/register.php
+++ app/login_screen/PHP_Login_screen/register.php
@@ -21,20 +21,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
      $email = $_POST['email'] ?? '';
      
 
-     if (empty($nome) || empty($cargo) || empty($senha)) || empty($email){
+     if (empty($nome) || empty($cargo) || empty($senha) || empty($email)){
 
         $erro="Preencha todos os campos";
-     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+     } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
         $erro = "O email está invalido";
      } else {
 
 
-     $stmt = $conn->prepare("SELECT id_funcionario FROM funcionarios WHERE email_funcionario=?")
-     }
-       
+     $stmt = $conn->prepare("SELECT id_funcionario FROM funcionarios WHERE email_funcionario=?");
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $result = $stmt->get_result();
 
+if ($result->num_rows > 0){
+   $erro = "Esse email já está cadastrado no sistema";
+}else { 
+   $hashSenha = password_hash($senha, PASSWORD_DEFAULT);
+   $stmt = $conn->prepare("INSERT INTO funcionarios (nome_funcionario, email_funcionario, senha_hash, cargo_funcionario) VALUES (?, ?, ?, ?)");
+      $stmt->bind_param("ssss", $nome, $email, $hashSenha, $cargo);
+      if ($stmt->execute()) {
+        $sucesso = "Funcionário cadastrado com sucesso!";
+        $_POST = []; // Limpa os campos
+      } else {
+        $erro = "Erro ao cadastrar funcionário: " . $conn->error;
+      }
 
+}
+}
 }
 
 
+
+if (!empty($erro)) {
+  echo "<script>alert('$erro'); window.history.back();</script>";
+}
+if (!empty($sucesso)) {
+  echo "<script>alert('$sucesso'); window.location.href = 'cadastro.html';</script>";
+}
 ?>
